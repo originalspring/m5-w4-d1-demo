40b985e0dfb5415ee5975e2d939053634fdef294
originalspring <105029639+originalspring@users.noreply.github.com>
2022-09-19 19:45:20 -0700

    new

---
+++ demo3/on-click.js
@@ -0,0 +1,77 @@
+"use strict";
+
+class OnClickElements extends React.Component {
+  constructor() {
+    super();
+  }
+
+  //add handler
+  handleClick(evt){
+    alert(`Hey! You clicked: ${evt.target.id}`)
+  }
+
+  render() {
+    // TODO: Your task is to add onClick listeners to each of the elements within the container-div <div>
+    return (
+      <div className="container-div">
+        <div id="div-element" onClick = { (e) => this.handleClick(e)} > 
+          I am DIV
+        </div>
+
+        <span id="span-element" onClick = { (e) => this.handleClick(e)}>
+          I am SPAN
+        </span>
+        <br></br>
+
+        <button id="button-element" onClick = { (e) => this.handleClick(e)}>
+          I am Button
+        </button>
+        <br></br>
+
+        <a id="link-element" href=""   className="button" onClick = { (e) => this.handleClick(e)}>
+          I am LINK
+        </a>
+
+        <div
+          id="div-element-2"
+          className="button"
+          onClick = { (e) => this.handleClick(e)}
+          >
+          I am DIV
+        </div>
+
+        <span
+          id="span-element-2"
+          className="button"
+          
+        >
+          I am SPAN
+        </span>
+        <br></br>
+
+        <button
+          id="button-element-2"
+          className="button"
+          
+        >
+          I am Button
+        </button>
+        <br></br>
+
+        <a
+          id="link-element-2"
+          className="button"
+          href=""
+          
+        >
+          I am LINK
+        </a>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  React.createElement(OnClickElements),
+  document.getElementById("root")
+);
